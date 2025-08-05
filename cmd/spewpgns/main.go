7c09cc62bad8d01c4a59e02fc29ef4f4dc0aaf3e
Russ Daniels <russd@inquest.com>
2025-08-05 14:05:20 -0700

    simple command to write pgns to bus

--- cmd/spewpgns/main.go
+++ cmd/spewpgns/main.go
@@ -77,7 +77,7 @@ func sendTestPGNs(ctx context.Context, publisher pgn.Publisher, log *logrus.Logg
 	ticker := time.NewTicker(2 * time.Second)
 	defer ticker.Stop()
 
-	sourceId := uint8(100) // Use a fixed source ID for testing
+	sourceId := uint8(254) // Use reserved address for nodes unable to claim an address
 	var counter float32 = 0
 
 	for {
@@ -87,20 +87,20 @@ func sendTestPGNs(ctx context.Context, publisher pgn.Publisher, log *logrus.Logg
 			return
 		case <-ticker.C:
 			counter++
-			
+
 			// Generate and send different types of PGNs
 			if err := sendEngineData(&publisher, sourceId, counter, log); err != nil {
 				log.Errorf("Failed to send engine data: %v", err)
 			}
-			
+
 			if err := sendSpeedData(&publisher, sourceId, counter, log); err != nil {
 				log.Errorf("Failed to send speed data: %v", err)
 			}
-			
+
 			if err := sendPositionData(&publisher, sourceId, counter, log); err != nil {
 				log.Errorf("Failed to send position data: %v", err)
 			}
-			
+
 			log.Infof("Sent test PGNs batch %d", int(counter))
 		}
 	}
@@ -112,7 +112,7 @@ func sendEngineData(publisher *pgn.Publisher, sourceId uint8, counter float32, l
 	// Boost pressure in Pascal (150 kPa = 150000 Pa)
 	boostPressurePa := float32(150000.0 + 30000.0*math.Sin(float64(counter)*0.2))
 	boostPressure := units.NewPressure(units.Pa, boostPressurePa)
-	
+
 	enginePgn := pgn.EngineParametersRapidUpdate{
 		Info: pgn.MessageInfo{
 			PGN:      pgn.EngineParametersRapidUpdatePgn,
@@ -124,7 +124,7 @@ func sendEngineData(publisher *pgn.Publisher, sourceId uint8, counter float32, l
 		BoostPressure: &boostPressure,
 		TiltTrim:      nil, // Not applicable for this test
 	}
-	
+
 	log.Debugf("Sending engine data: RPM=%.1f, Boost=%.1f kPa", rpm, boostPressurePa/1000.0)
 	return publisher.Write(enginePgn)
 }
@@ -133,25 +133,25 @@ func sendSpeedData(publisher *pgn.Publisher, sourceId uint8, counter float32, lo
 	// Generate realistic boat speeds (5-15 knots)
 	speedKnots := 10.0 + 3.0*math.Sin(float64(counter)*0.15)
 	speedMs := float32(speedKnots * 0.514444) // Convert knots to m/s
-	
+
 	waterSpeed := units.NewVelocity(units.MetersPerSecond, speedMs)
 	groundSpeed := units.NewVelocity(units.MetersPerSecond, speedMs+0.2) // Slight current
 	sid := uint8(1)
 	direction := uint8(0) // Forward
-	
+
 	speedPgn := pgn.Speed{
 		Info: pgn.MessageInfo{
 			PGN:      pgn.SpeedPgn,
 			SourceId: sourceId,
 			TargetId: 255, // Broadcast
 		},
-		Sid:                        &sid,
-		SpeedWaterReferenced:       &waterSpeed,
-		SpeedGroundReferenced:      &groundSpeed,
-		SpeedWaterReferencedType:   pgn.PaddleWheel,
-		SpeedDirection:             &direction,
+		Sid:                      &sid,
+		SpeedWaterReferenced:     &waterSpeed,
+		SpeedGroundReferenced:    &groundSpeed,
+		SpeedWaterReferencedType: pgn.PaddleWheel,
+		SpeedDirection:           &direction,
 	}
-	
+
 	log.Debugf("Sending speed data: %.1f knots (%.2f m/s)", speedKnots, speedMs)
 	return publisher.Write(speedPgn)
 }
@@ -161,15 +161,15 @@ func sendPositionData(publisher *pgn.Publisher, sourceId uint8, counter float32,
 	// Starting position: approximately San Francisco Bay
 	baseLat := 37.7749
 	baseLon := -122.4194
-	
+
 	// Create a small circular motion (radius ~100 meters)
 	angle := float64(counter) * 0.1
 	deltaLat := 0.001 * math.Cos(angle) // About 111 meters per 0.001 degrees
 	deltaLon := 0.001 * math.Sin(angle)
-	
+
 	latitude := baseLat + deltaLat
 	longitude := baseLon + deltaLon
-	
+
 	positionPgn := pgn.PositionRapidUpdate{
 		Info: pgn.MessageInfo{
 			PGN:      pgn.PositionRapidUpdatePgn,
@@ -179,7 +179,7 @@ func sendPositionData(publisher *pgn.Publisher, sourceId uint8, counter float32,
 		Latitude:  &latitude,
 		Longitude: &longitude,
 	}
-	
+
 	log.Debugf("Sending position data: %.6f, %.6f", latitude, longitude)
 	return publisher.Write(positionPgn)
 }
